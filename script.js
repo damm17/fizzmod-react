8edf14f9b9eccde81691933a9abfc55b6b8545aa
Damian Bertazza <damian.bertazza@gmail.com>
2019-08-04 15:12:03 -0300

    Ejercicio de Eventos - Clase 3

--- script.js
+++ script.js
@@ -1,102 +1,34 @@
-// CLASE 02
-// TIPOS DE DATOS
-
-// undefined : una variable sin valor
-// NULL : "referencia" a ningún objeto
-
-// Primitivos: Se pasan por valor
-// String : "" - '' - ``
-// Boolean : true o false
-//  True o {False|0|NULL|""|undefined}
-// Number
-
-// Objetos: Se pasan por referencia. Todos son matrices asociativas.
-// -Object
-// -Array
-// -Function
-
-
-
-/* WINDOW API
-outer{width|height} => Number - medidas en pixeles del navegador
-inner{width|height} => Number - medidas en pixeles del DOM
-screen.{width|height} => Number - medidas de hardware
-location => Object - Nos permite jugar con la barra de dirección
-history.{back|forward|go(Number)} => Object - Nos permite jugar con el historial de la sesión
-*/
-
-// console.dir(Any) => Nos muestra una variable en formato Json
-// console.table(Any) => Nos muestra una variable en formato tabular
-// console.time("Label") | console.timeEnd("Label") => Para medir tiempo de ejecución
-// console.error() | console.warn() 
-
-/* DOM = Document Object Model
-// Creación de Nodo (Etiqueta) => document.createElement("div")
-// Selección de Nodo => document.getElementById()|getElementsByClassName()|getElementsByTagName() [Los dos ultimos devuelven un array]
-// document.querySelector()|document.querySelectorAll() -> selectores de CSS
-*/
-
-// CLASE 03
-/* 
-var a = document.createElement("a");
-a.innerHTML = "Hola Mundo";
-// XSS = Cross-Site Scripting => Ataque JS con el innerHTML
-
-a.innerText = "lo que sea"; => Agrega texto plano
-//NodoTarget.appendChild(nuevoNodo);
-document.body.appendChild(a);
-
-a.dataset => seleccionar los "data-" definidos en html
-
-var a_copia = a.cloneNode(true); => Hacer una copia entera o parcial de un nodo.
-
-*/
-
-/* 
-EVENTOS 
-
-a.addEventListener("click", function(e){
-    console.log("asd");
-    e.stopPropagation();
-});
-
-a.addEventListener("click", (e)=>{
-    console.log("asd");
-    e.preventDefault();
-});
-*/
-
 let a = document.querySelector("a");
 a.addEventListener("click", (e)=>{
     e.preventDefault();
     let p = document.createElement("p");
-    p.innerText = "Está seguro que quiere abandonar la página?";
     let div = document.createElement("div");
     let button_a = document.createElement("button");
-    button_a.innerText = "Aceptar";
-    button_a.id = "aceptar";
     let button_b = document.createElement("button");
+
+    p.innerText = "Está seguro que quiere abandonar la página?";
+    button_a.innerText = "Aceptar";
     button_b.innerText = "Cancelar";
-    button_b.id = "cancelar";
+    button_a.id = "aceptar";
 
     let f = document.createDocumentFragment();
     f.appendChild(p);
     f.appendChild(button_a);
     f.appendChild(button_b);
 
+    // Para prevenir que se creen múltiples divs con los botones, creo un condicional para chequear si ya existe uno de los botones en el DOM
+    let aceptar = document.getElementById("aceptar");
+    if (!aceptar) {
+        document.body.appendChild(div);
+        div.appendChild(f);
+    }
 
-    document.body.appendChild(div);
-    let div_dom = document.querySelector("div");
-    div_dom.appendChild(f);
+    button_a.addEventListener("click",()=>{
+        location.href = e.target.href;
+    });
 
-    let body = document.body;
-    body.addEventListener("click", e=>{
-        if (e.target.id == "aceptar") {
-            window.location.href ="https://google.com";
-            console.log("aasd");
-        } else if (e.target.id == "cancelar") {
-            document.body.removeChild(div);
-        }
+    button_b.addEventListener("click",()=>{
+        document.body.removeChild(div);
     });
 });
 
