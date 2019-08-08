eb3225dfacb4240ced390282f0e7d4cb1d93ce52
Damian Bertazza <damian.bertazza@gmail.com>
2019-08-08 19:31:01 -0300

    Clase 8

--- react/app/src/index.js
+++ react/app/src/index.js
@@ -1,15 +1,3 @@
-/*
-React se separa en : react y react-dom
-react te sirve para crear componentes. react-dom te sirve para que una vez que tengas un componente lo puedas inyectar en el dom
-cada componente va en su propio archivo
-
-1- Tiene que siempre estar la variable React en scope. (line 12)
-2- Un componente de React es una función.
-3- Todos los componentes de React tienen que retornar algo: Otro componente de React | String | DomNode (etiqueta)
-4- Todos los componentes de React van en Mayúscula
-5- Los componentes se escriben en JSX para ejecutarlos
-*/
-
 // ES6
 import React from "react"
 import ReactDOM from "react-dom"
